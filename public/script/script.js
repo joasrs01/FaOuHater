104140182a141f1af2d0d5335add1de8b4a0aff6
joasrs <joas_rsouza@outlook.com>
2024-09-02 23:09:34 -0300

    adicionado validações no formulario de cadastro de usuario e review

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,6 @@
-let btnsLike = document.querySelectorAll(".btn-like-dlike");
-
-console.log("arquivo style carregado");
+const btnsLike = document.querySelectorAll(".btn-like-dlike");
+const formUsuario = document.querySelector("#form-usuario");
+const formReview = document.querySelector("#form-review");
 
 btnsLike.forEach((btn) => {
   btn.addEventListener("click", async () => {
@@ -34,3 +34,113 @@ btnsLike.forEach((btn) => {
     }
   });
 });
+
+if (formUsuario) {
+  formUsuario.addEventListener("submit", onValidarFormUsuario);
+}
+
+console.log(formReview);
+if (formReview) {
+  formReview.addEventListener("submit", onValidarFormReview);
+}
+
+function onValidarFormReview(event) {
+  try {
+    let valid = true;
+    const musica = document.querySelector("#musica");
+    const artista = document.querySelector("#artista");
+    const review = document.querySelector("#txt-area-review");
+
+    musica.classList.remove("form-control-invalido");
+    artista.classList.remove("form-control-invalido");
+    review.classList.remove("form-control-invalido");
+
+    // musica
+    if (musica.value.trim() === "") {
+      valid = false;
+      musica.classList.add("form-control-invalido");
+    }
+
+    // artista
+    if (artista.value.trim() === "") {
+      valid = false;
+      artista.classList.add("form-control-invalido");
+    }
+
+    // review
+    if (review.value.trim() === "") {
+      valid = false;
+      review.classList.add("form-control-invalido");
+    }
+
+    if (!valid) {
+      event.preventDefault();
+    }
+  } catch (error) {
+    console.log("erro ao executar a validação do formulario: " + error);
+  }
+}
+
+function onValidarFormUsuario(event) {
+  try {
+    let valid = true;
+    const name = document.querySelector("#nome");
+    const email = document.querySelector("#email");
+    const login = document.querySelector("#login");
+    const senha = document.querySelector("#senha");
+
+    const spanMsgEmail = document.querySelector("#msg-email");
+    const spanMsgLogin = document.querySelector("#msg-login");
+
+    //remove invalidações se tiver
+    spanMsgEmail.innerHTML = "";
+    spanMsgLogin.innerHTML = "";
+
+    name.classList.remove("form-control-invalido");
+    email.classList.remove("form-control-invalido");
+    login.classList.remove("form-control-invalido");
+    senha.classList.remove("form-control-invalido");
+
+    // Nome
+    if (name.value.trim() === "") {
+      valid = false;
+      name.classList.add("form-control-invalido");
+    }
+
+    // Email
+    const emailPattern =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+    if (email.value.trim() === "") {
+      valid = false;
+      email.classList.add("form-control-invalido");
+    } else if (!emailPattern.test(email.value.trim())) {
+      valid = false;
+      email.classList.add("form-control-invalido");
+      spanMsgEmail.innerHTML = "O e-mail digitado não é valido.";
+    }
+
+    // Login
+    const loginPattern = /^[a-zA-Z0-9_]+$/; // Alfanumérico e underline permitido
+    if (login.value.trim() === "") {
+      valid = false;
+      login.classList.add("form-control-invalido");
+    } else if (!loginPattern.test(login.value.trim())) {
+      valid = false;
+      email.classList.add("form-control-invalido");
+      spanMsgLogin.innerHTML =
+        "O login não pode conter espaços ou caracteres especiais.";
+    }
+
+    // Nome
+    if (senha.value.trim() === "") {
+      valid = false;
+      senha.classList.add("form-control-invalido");
+    }
+
+    if (!valid) {
+      event.preventDefault();
+    }
+  } catch (error) {
+    console.log("erro ao executar a validação do formulario: " + error);
+  }
+}
